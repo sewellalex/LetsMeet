69305b4cc7d290fc39fa4d6654b71326cc2c8438
sewellalex <alex.g.sewell@gmail.com>
2015-10-21 10:50:49 -0700

    check in working map

--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@
  */
 
 function getResults() {
+  // create a new instance of GeoCoder to get users latitude and longitude
   var yourGeoCoder = new google.maps.Geocoder();
   var yourAddress = document.getElementById("yourAddress").value;
   var yourLocation;
@@ -11,23 +12,29 @@ function getResults() {
     {
       yourLocation = results[0].geometry.location;
     }
+
+  // create a new instance of GeoCoder to get user's friend's latitude and longitude
   var friendsGeoCoder = new google.maps.Geocoder();
   var friendsAddress = document.getElementById('friendsAddress').value;
   var friendsLocation;
-    friendsGeoCoder.geocode( { 'address': friendsAddress}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK)
-      {
-        friendsLocation = results[0].geometry.location;
-      }
+  friendsGeoCoder.geocode( { 'address': friendsAddress}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK)
+    {
+      friendsLocation = results[0].geometry.location;
+    }
+
+  // set start point to users location; set end point to user's friend's location
   var startPoint = yourLocation;
   var endPoint = friendsLocation;
 
+  // create a new instance of the map
   var map = new google.maps.Map(document.getElementById('map'), {
     center: startPoint,
     scrollwheel: true,
     zoom: 16
   });
 
+  // creates rendering of direction
   var directionsDisplay = new google.maps.DirectionsRenderer({
     map: map
   });
@@ -45,6 +52,7 @@ function getResults() {
     if (status == google.maps.DirectionsStatus.OK) {
       // Display the route on the map.
       directionsDisplay.setDirections(response);
+      console.log(directionsService);
     }
   });
   });
