f0c318b60a24335f43ef3e54a08583bef53ba91d
sewellalex <alex.g.sewell@gmail.com>
2015-10-23 16:26:32 -0700

    dynamically created results from yelp api onto page

--- js/show-results.js
+++ js/show-results.js
@@ -2,6 +2,47 @@
  * Created by alexsewell on 10/22/15.
  */
 
-function showResults(name, image, address, rating) {
+function showResults(name, image, addressOne, addressTwo, rating) {
+  var displayRestaurants = document.getElementById('restaurants');
+  //for (var create = 0; create < 4; create++) {
 
+  // Create four columns
+  var createCol = document.createElement('div');
+  createCol.setAttribute('class', 'col-md-3');
+  displayRestaurants.appendChild(createCol);
+
+  // Create restaurant name
+  var createHeader = document.createElement('div');
+  createHeader.setAttribute('class', 'restaurant-name');
+  createCol.appendChild(createHeader);
+  var createName = document.createTextNode(name);
+  createHeader.appendChild(createName);
+
+  // Create restaurant image
+  var createImage = document.createElement('img');
+  createImage.setAttribute('class', 'restaurant-image img-circle center-block');
+  createImage.setAttribute('src', image);
+  createCol.appendChild(createImage);
+
+  // Create restaurant address
+  var createAddress = document.createElement('div');
+  createCol.appendChild(createAddress);
+  var houseNumber = document.createElement('p');
+  houseNumber.setAttribute('class', 'restaurant-address');
+  createAddress.appendChild(houseNumber);
+  var houseNumberText = document.createTextNode(addressOne);
+  houseNumber.appendChild(houseNumberText);
+  var address = document.createElement('p');
+  address.setAttribute('class', 'restaurant-address');
+  createAddress.appendChild(address);
+  var addressNumberText = document.createTextNode(addressTwo);
+  address.appendChild(addressNumberText);
+
+  // Create share link
+  var shareLink = document.createElement('a');
+  shareLink.setAttribute('class', 'share-location center-block');
+  shareLink.setAttribute('href', "mailto:?subject=Let's meet in the middle!&body=Hi, Let's meet in the middle at " + name + ". The address is " + addressOne + " " + addressTwo + "!");
+  createCol.appendChild(shareLink);
+  var shareText = document.createTextNode('Share with Friend!');
+  shareLink.appendChild(shareText);
 }
--- js/yelp-results.js
+++ js/yelp-results.js
@@ -7,21 +7,15 @@ function yelpResults(coordinates) {
   request.send();
   request.addEventListener('load', function() {
     var getData = JSON.parse(request.responseText);
-    console.log(getData);
     var businessesArray = getData.businesses;
-    console.log(businessesArray);
     for (var business = 0; business < businessesArray.length; business++) {
       var restaurantName = businessesArray[business].name;
-      console.log(restaurantName);
       var restaurantImage = businessesArray[business].image_url;
-      console.log(restaurantImage);
-      for (var address = 0; address < businessesArray[business].location.display_address.length; address++) {
-        var restaurantAddress = businessesArray[business].location.display_address[address];
-        console.log(restaurantAddress);
-      }
+      var restaurantAddressOne = businessesArray[business].location.display_address[0];
+      var restaurantAddressTwo = businessesArray[business].location.display_address[1];
       var restaurantRating = businessesArray[business].rating_img_url_large;
-      console.log(restaurantRating);
+
+      showResults(restaurantName, restaurantImage, restaurantAddressOne, restaurantAddressTwo, restaurantRating);
     }
-    //showResults(restaurantName, restaurantImage, restaurantAddress, restaurantRating);
   });
 }
